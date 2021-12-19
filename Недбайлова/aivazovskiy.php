a4c085296c9d3d0277769144df310c96359caa9e
sofianedbajlova <96359597+sofianedbajlova@users.noreply.github.com>
2021-12-19 03:49:03 +0300

    загружаем файлы сайта

---
+++ Недбайлова/aivazovskiy.php
@@ -0,0 +1,47 @@
+<html  lang="en"> 
+<head>  
+<meta charset="utf-8">
+	<title>Иван Константинович Айвазовский</title> 
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head> 
+<body style="background-color:black;"> 
+<div class="header">
+<a class="active"  href="aivazovskiy.php" class="logo"><img src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Jawlensky_autograph.png;" width="100" height="30" alt="подпись И.К.Айвазовского"></a>
+<div class="header-right">
+<a href="aivazovskiy1.php">Биография</a>
+<a href="aivazovskiy2.php">Творчество</a>
+<a href="aivazovskiy3.php">Награды</a>
+</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1 style="color:white;text-align:center;font-family:courier;"><ins>Иван Константинович Айвазовский</ins></h1> 
+<table width="85%" cellspacing="80%" cellpadding="60%"> 
+<tr> 
+<td class="leftcool"><img src="https://painteropedia.ru/wp-content/uploads/2014/07/aivazovsky.jpg" width="440" height="500" alt="портрет И.К.Айвазовского"></td> 
+<td style="color:white;font-family:courier;"><i><b>Иван Константинович Айвазовский (1817-1900)</b> </i>— великий русский художник армянского происхождения. Он был меценатом, а также почетным членом римской, парижской и амтердамской Академий художеств.<br> 
+Наиболее известен как художник – маринист, автор картин на морскую и военную тематику. 
+<br>
+<br>
+<br>
+<ins>Разделы сайта:</ins>
+<br>
+<ul>
+<li><a href="aivazovskiy1.php" style="color:grey">Биография художника</a></li>
+<li><a href="aivazovskiy2.php" style="color:grey">Творчество И.К.Айвазовского</li>
+<li><a href="aivazovskiy3.php" style="color:grey">Основные награды</li>
+</ul>
+</td> 
+</tr> 
+</table> 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<hr>
+</html>
